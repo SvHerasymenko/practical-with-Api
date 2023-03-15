e2f98f084d2882b0a088540b9e230566b867f64e
Admin <admin@MacBook-Pro-Admin.local>
2023-03-15 20:36:00 +0200

    boor_file_done

--- book.py
+++ book.py
@@ -1,2 +1,33 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
+bookname= 'Sherlock Holmes'
+
+def bookinfo(bookname):
+    url =f'https://www.googleapis.com/books/v1/volumes?q={bookname}+intitle:keyes&key=AIzaSyCE1fuJ62Yr3qi_6xKcNt7cmgUh_WxXHA8'
+    res=requests.get(url).json()
+
+    if res['totalItems'] != 0:
+        for items in res['items']:
+
+            try:print('~~~'+items['volumeInfo']['title']+'~~~\n')
+            except:...
+
+            try:
+                for authors in items['volumeInfo']['authors'] :
+                    print('Authors: '+ authors)
+            except:...
+
+            try:print('PublishedDate: ' + items['volumeInfo']['publishedDate'])
+            except:...
+
+            try:print('PageCount: '+ str(items['volumeInfo']['pageCount']))
+            except:...
+
+            try:print('InfoLink: ' + items['volumeInfo']['infoLink'])
+            except:...
+    else:
+        print('Помилка! Введіть назву праивльно!')
+
+bookinfo(bookname)
