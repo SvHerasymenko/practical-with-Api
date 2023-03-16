eb79156c9be9ddc0c7129adc2306cb1090e23cb3
Admin <admin@MacBook-Pro-Admin.local>
2023-03-16 22:21:56 +0200

    create restorans.py

--- book.py
+++ book.py
@@ -1,6 +1,4 @@
 import requests
-import json
-from bs4 import BeautifulSoup
 
 bookname=str(input('Enter bookname:'))
 
---
+++ restorans.py
@@ -0,0 +1,32 @@
+import requests 
+import json
+
+url = "https://api.yelp.com/v3/businesses/search?location=New York City&categories=&sort_by=best_match"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer XCGoXidrY1S0p2jYe4trfR2bEZjr3hkfXgvjX5yUUCrn99X3wj-4xx0jKYwCfTC_PFQF1dXXuhyh7k40U0NI1NuPBSt7y9lsUw67XxBYhz9IBjr5OR6MTlhiHWwTZHYx"
+}
+
+response = requests.get(url, headers=headers).json()
+
+json = json.dumps(response, indent=4)
+
+restInfo=[]
+
+try:
+    data= response['businesses']
+    for items in data:
+        print('\n~~~'+ items['name'] +'~~~\n')
+        print('Rating: '+ str(items['rating']))
+        print('Location: '+items['location']['address1'])
+        print('Review_count: '+str(items['review_count']))
+        print('Phone: '+items['phone'])
+        print('Url: '+items['url'])
+        print("Category:")
+        for categories in items['categories']:
+            print(categories['title'])
+ 
+except:...
+
+
