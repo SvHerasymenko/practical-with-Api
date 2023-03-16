be3d45611969f7bf0ea27838493f3bdde02bd461
Admin <admin@MacBook-Pro-Admin.local>
2023-03-16 22:36:03 +0200

    edit weather.py

--- restorans.py
+++ restorans.py
@@ -1,32 +1,35 @@
 import requests 
 import json
 
-url = "https://api.yelp.com/v3/businesses/search?location=New York City&categories=&sort_by=best_match"
+cityname= str(input('Enter city name: '))
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer XCGoXidrY1S0p2jYe4trfR2bEZjr3hkfXgvjX5yUUCrn99X3wj-4xx0jKYwCfTC_PFQF1dXXuhyh7k40U0NI1NuPBSt7y9lsUw67XxBYhz9IBjr5OR6MTlhiHWwTZHYx"
-}
+def resttoransInfo(cityname)
+    url = f"https://api.yelp.com/v3/businesses/search?location={cityname}&categories=&sort_by=best_match"
 
-response = requests.get(url, headers=headers).json()
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer XCGoXidrY1S0p2jYe4trfR2bEZjr3hkfXgvjX5yUUCrn99X3wj-4xx0jKYwCfTC_PFQF1dXXuhyh7k40U0NI1NuPBSt7y9lsUw67XxBYhz9IBjr5OR6MTlhiHWwTZHYx"
+    }
 
-json = json.dumps(response, indent=4)
+    response = requests.get(url, headers=headers).json()
 
-restInfo=[]
+    json = json.dumps(response, indent=4)
 
-try:
-    data= response['businesses']
-    for items in data:
-        print('\n~~~'+ items['name'] +'~~~\n')
-        print('Rating: '+ str(items['rating']))
-        print('Location: '+items['location']['address1'])
-        print('Review_count: '+str(items['review_count']))
-        print('Phone: '+items['phone'])
-        print('Url: '+items['url'])
-        print("Category:")
-        for categories in items['categories']:
-            print(categories['title'])
- 
-except:...
+    restInfo=[]
 
+    try:
+        data= response['businesses']
+        for items in data:
+            print('\n~~~'+ items['name'] +'~~~\n')
+            print('Rating: '+ str(items['rating']))
+            print('Location: '+items['location']['address1'])
+            print('Review_count: '+str(items['review_count']))
+            print('Phone: '+items['phone'])
+            print('Url: '+items['url'])
+            print("Category:")
+            for categories in items['categories']:
+                print(categories['title'])
+    
+    except:print('Enter the city name correctly')
 
+resttoransInfo(cityname)
--- weather.py
+++ weather.py
@@ -1,10 +1,17 @@
 import requests
+import json
 
 city=str(input("Enter your city: "))
 
-key='69804c504a3bb0ba7fb3f8d3d311375c'
 
-def weather_check(key,city):
+
+def weather_check(city):
+    key='69804c504a3bb0ba7fb3f8d3d311375c'
     url='http://api.openweathermap.org/data/2.5/find?q={city}&type=like&APPID={key}'
-    res= requests.get(url).json() 
-    print(json)
+    res= requests.get(url)
+    try:
+        data = {'Weather': res['weather'][0]['main'], 'Description': res['weather'][0]['description'], 'Curent temp': res['main']['temp'], 'Temp_min': res['main']['temp_min'], 'Temp_max': res['main']['temp_max']'}
+        print(pd.DataFrame([data]))
+    except:print('Enter the city name correctly')
+    
+weather_check(city)
